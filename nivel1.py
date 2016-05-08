2741c7330a19487ad62184bcaaacda29784c5bcb
karen-lopez <stefanny0214@gmail.com>
2016-05-08 15:36:23 -0500

    mapa del nive1 v.o.3

--- nivel1.py
+++ nivel1.py
@@ -1,10 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
- 
 import pygame
- 
-"""
-Constantes globales
-"""
  
 # Colores
 NEGRO = (0, 0, 0) 
@@ -15,70 +12,60 @@ VERDE = (0, 255, 0)
 CELESTE = (153, 255, 204) 
 PURPURA= (61, 6, 49)
 
-#  Dimensiones de la pantalla
 LARGO_PANTALLA  = 800
 ALTO_PANTALLA = 700
  
 class Protagonista(pygame.sprite.Sprite): 
     """ Esta clase representa la barra inferior que controla el protagonista. """
-   
-    # -- Atributos 
-    # Establecemos el vector velocidad del protagonista
     cambio_x = 0
-    cambio_y = 0
-     
-     # Lista de todos los sprites contra los que podemos botar
-    nivel = None
-     
-
+    cambio_y = 0 
+    
+    Nivel = None     
+ 
     def __init__(self): 
 
-         
-        # Llama al constructor padre 
-        pygame.sprite.Sprite.__init__(self) 
-       
+        pygame.sprite.Sprite.__init__(self)         
         largo = 40
         alto = 50
-	triangulo=pygame.Surface([largo, alto])
-        self.image = triangulo
+        self.image = pygame.Surface([largo, alto])
         self.image.fill(ROJO)        
-        # Establecemos una referencia hacia la imagen rectangular.
-        self.rect = self.image.get_rect()
-	
+        self.rect = self.image.get_rect() 
        
     def update(self): 
-
-        # Gravedad
-        self.calc_grav()
-         
-        # Desplazar izquierda/derecha
+        """ Desplazamos al protagonista.  """
+        self.calc_grav()         
         self.rect.x += self.cambio_x
-	#self.rect.y += (abs(self.cambio_x)/2)*-1
          
-        # Comprobamos si hemos chocado contra algo
+
         lista_impactos_bloques = pygame.sprite.spritecollide(self, self.nivel.listade_plataformas, False)
         for bloque in lista_impactos_bloques:
-            # Si nos estamos desplazando hacia la derecha, hacemos que nuestro lado derecho sea el lado         
-            # izquierdo del objeto que hemos tocado
+            
             if self.cambio_x > 0:
                 self.rect.right = bloque.rect.left
             elif self.cambio_x < 0:
-                # En caso contrario, si nos desplazamos hacia la izquierda, hacemos lo opuesto.
+
                 self.rect.left = bloque.rect.right
  
-        # Desplazar arriba/abajo
+
         self.rect.y += self.cambio_y
          
-        # Comprobamos si hemos chocado contra algo
+
         lista_impactos_bloques = pygame.sprite.spritecollide(self, self.nivel.listade_plataformas, False) 
         for bloque in lista_impactos_bloques:
+ 
+
             if self.cambio_y > 0:
                 self.rect.bottom = bloque.rect.top 
             elif self.cambio_y < 0:
                 self.rect.top = bloque.rect.bottom
+ 
             # Detenemos nuestro movimiento vertical
             self.cambio_y = 0
+             
+            if isinstance(bloque, PlataformaEnMovimiento):
+                self.rect.x += bloque.cambio_x
  
+
     def calc_grav(self):
         """ Calculamos el efecto de la gravedad.  """
         if self.cambio_y == 0:
@@ -86,46 +73,48 @@ class Protagonista(pygame.sprite.Sprite):
         else:
             self.cambio_y += .35
  
-        #Observamos si nos encontramos sobre el suelo. 
+        # Observamos si nos encontramos sobre el suelo. 
         if self.rect.y >= ALTO_PANTALLA - self.rect.height and self.cambio_y >= 0:
             self.cambio_y = 0
             self.rect.y = ALTO_PANTALLA - self.rect.height
  
-    def saltar(self):
-		
+
+    def jump(self):
+        
         self.rect.y += 2
         lista_impactos_plataforma = pygame.sprite.spritecollide(self, self.nivel.listade_plataformas, False)
-        self.rect.y -= 2        
-	
-	#comprueba si choca en la parte de abajo con el piso o un bloque
+        self.rect.y -= 2
+         
+        
         if len(lista_impactos_plataforma) > 0 or self.rect.bottom >= ALTO_PANTALLA:
-	    #salta si no hay ostaculos
             self.cambio_y = -10
              
-
+   
     def ir_izquierda(self):
         """ Es llamado cuando el usuario pulsa la flecha izquierda  """
         self.cambio_x = -6
  
     def ir_derecha(self):
-        """ Es llamado cuando el usuario pulsa la flecha derecha  """
+        """ Es llamado cuando el usuario pulsa la flecha  derecha """
         self.cambio_x = 6
  
     def stop(self):
-        """ Es llamado cuando el usuario abandona el teclado """
+       
         self.cambio_x = 0
-
-
-                    
-class Plataforma(pygame.sprite.Sprite):
-    """ Plataforma sobre la que el usuario puede saltar """
  
+
+                   
+class Plataforma(pygame.sprite.Sprite):
+    """ Plataforma sobre la que el usuario puede saltar. """
     def __init__(self, largo, alto ):
-      
-        pygame.sprite.Sprite.__init__(self)        
+       
+        pygame.sprite.Sprite.__init__(self)
+         
         self.image = pygame.Surface([largo, alto])
-        self.image.fill(CELESTE)               
+        self.image.fill(CELESTE)    
+                 
         self.rect = self.image.get_rect()
+
 
 class PlataformaColor(pygame.sprite.Sprite):
     """ Plataforma sobre la que el usuario puede saltar """
@@ -137,57 +126,115 @@ class PlataformaColor(pygame.sprite.Sprite):
         self.image.fill(color)               
         self.rect = self.image.get_rect()
 
-
+  
+ 
+class PlataformaEnMovimiento(Plataforma): 
+    cambio_x = 0
+    cambio_y = 0
+      
+    limite_superior = 0
+    limite_inferior = 0
+    limite_izquierda = 0
+    limite_derecha = 0
+     
+    protagonista = None
+     
+    Nivel = None
+     
+    def update(self):        
+        # Desplazar izquierda/derecha
+        self.rect.x += self.cambio_x
+         
+        # Comprobamos si hemos chocado contra el protagonista
+        impacto = pygame.sprite.collide_rect(self, self.protagonista)
+        if impacto:
+            # Hemos impactado contra el protagonista. Lo empujamos a un lado
+            # y asumimos que no impactará con ninguna otra cosa.
+             
+            if self.cambio_x < 0:
+                self.protagonista.rect.right = self.rect.left
+            else:
+                # En caso contrario (desplazamiento a la izquierda), hacemos lo opuesto
+                self.protagonista.rect.left = self.rect.right
+ 
+        # Desplazar arriba/abajo
+        self.rect.y += self.cambio_y
+         
+        # Comprobamos si hemos impactado con el protagonista
+        impacto = pygame.sprite.collide_rect(self, self.protagonista)
+        if impacto:
+           # Hemos impactado contra el protagonista. Lo empujamos a un lado
+           # y asumimos que no impactará con ninguna otra cosa.
+             
+           # Restablecemos nuestra posición basándonos en la parte superior/inferior
+       # del objeto
+            if self.cambio_y < 0:
+                self.protagonista.rect.bottom = self.rect.top 
+            else:
+                self.protagonista.rect.top = self.rect.bottom
+ 
+        # Comprobamos los límites y vemos si es necesario invertir el sentido
+ 
+        if self.rect.bottom > self.limite_inferior or self.rect.top < self.limite_superior:
+            self.cambio_y *= -1
+             
+        cur_pos = self.rect.x - self.nivel.desplazar_escenario
+        if cur_pos < self.limite_izquierda or cur_pos > self.limite_derecha:
+            self.cambio_x *= -1
  
 class Nivel():
-   
      
     def __init__(self, protagonista):
-        """ Constructor.  Requerido para cuando las plataformas
-            que se desplazan colisionan con el protagonista. """
         self.listade_plataformas = pygame.sprite.Group()
         self.listade_enemigos = pygame.sprite.Group()
         self.protagonista = protagonista
-        self.desplazar_escenario = 0
+         
+        # Imagen de fondo
+        self.imagende_fondo = None
+         
+        # Cuán lejos a la izquierda/derecha se ha desplazado el escenario
+	self.desplazar_escenario = 0
+	self.limitedel_nivel = -1000
      
-    # Actualizamos todo en este nivel
+    # update todo en este Nivel
     def update(self):
-        """ Actualizamos todo en este nivel."""
+        """ update todo en este Nivel."""
         self.listade_plataformas.update()
         self.listade_enemigos.update()
      
     def draw(self, pantalla):
-        """ Dibujamos todo en este nivel. """
+        """ draw todo en este Nivel. """
         pantalla.fill(PURPURA)
         self.listade_plataformas.draw(pantalla)
         self.listade_enemigos.draw(pantalla)
          
     def escenario_desplazar(self, desplazar_x):
-        """ Para cuando el usuario se desplaza a la izquierda/derecha y necesitamos mover 
-        todo: """
+        """ Cuando el usuario se mueve de izquierda/derecha y necesitamos que todo se desplace: """
+         
+        # Llevamos la cuenta de la cantidad de desplazamiento
         self.desplazar_escenario += desplazar_x
          
-        for plataforma in self.listade_plataformas:
-            plataforma.rect.x += desplazar_x
+        # Iteramos a través de todas las listas de sprites y desplazamos
+        for Plataforma in self.listade_plataformas:
+            Plataforma.rect.x += desplazar_x
              
         for enemigo in self.listade_enemigos:
             enemigo.rect.x += desplazar_x
-
-
      
-# Creamos las plataformas para el nivel
+# Creamos las Plataformas para el Nivel
 class Nivel_01(Nivel):
-
+    """ Definición para el Nivel 1. """
  
     def __init__(self, protagonista):
-        """ Creamos el nivel 1. """    
-
+        """ Crear Nivel 1. """
+         
+        # Llamada al constructor padre
         Nivel.__init__(self, protagonista)
-        self.limitedel_nivel = -1000
  
-	#ANCHO, ALTO, x, y
+        self.limitedel_nivel = -1500
+         
+        #ANCHO, ALTO, x, y
         nivel = [ [150, 35, 140, 625],
-                  [150, 35, 360, 500],
                   [150, 35, 0, 375],
                   [150, 35, 580, 625],
 		  [150, 35, 1000, 375],
@@ -200,6 +247,8 @@ class Nivel_01(Nivel):
 		   [220, 600, 1800, 100],
 		   ]
 
+         
+        # Iteramos a través del array anterior y añadimos Plataformas
         for plataforma in nivel:
             bloque = Plataforma(plataforma[0], plataforma[1])
             bloque.rect.x = plataforma[2]
@@ -212,48 +261,61 @@ class Nivel_01(Nivel):
             borde.rect.x = plataformaColor[2]
             borde.rect.y = plataformaColor[3]
             borde.protagonista = self.protagonista
-            self.listade_plataformas.add(borde)                             
+            self.listade_plataformas.add(borde)
+
+         
+        #Plataforma movil no.1
+					#ancho, alto
+	bloque = PlataformaEnMovimiento(150, 35) 
+        bloque.rect.x = 360
+        bloque.rect.y = 500
+        bloque.limite_izquierda = 360
+        bloque.limite_derecha = 700
+        bloque.cambio_x = 1
+        bloque.protagonista = self.protagonista
+        bloque.nivel = self
+        self.listade_plataformas.add(bloque)
+                         
  
-
-
-
+ 
 def main():
     """ Programa Principal """
-    pygame.init()  
-	
+    pygame.init() 
+        
+    # Establecemos el alto y largo de la pantalla 
     dimensiones = [LARGO_PANTALLA, ALTO_PANTALLA] 
-    pantalla = pygame.display.set_mode(dimensiones, pygame.FULLSCREEN)     
-    pygame.display.set_caption("NIVEL 1") 
+    pantalla = pygame.display.set_mode(dimensiones) 
+       
+    pygame.display.set_caption("Plataformer y Plataformas en Movimiento") 
      
-
+    # Creamos al protagonista
     protagonista = Protagonista()
  
-    # Creamos todos los niveles
+    # Creamos todos los Niveles
     listade_niveles = []
-    listade_niveles.append(Nivel_01(protagonista))
-
+    listade_niveles.append( Nivel_01(protagonista) )
      
-    # Establecemos el nivel actual
+    # Establecemos el Nivel actual
     nivel_actual_no = 0
     nivel_actual = listade_niveles[nivel_actual_no]
      
     listade_sprites_activas = pygame.sprite.Group()
     protagonista.nivel = nivel_actual
      
-    protagonista.rect.x = 0
+    protagonista.rect.x = 340
     protagonista.rect.y = ALTO_PANTALLA - protagonista.rect.height
     listade_sprites_activas.add(protagonista)
          
-
+    # Iteramos hasta que el usuario hace click sobre el botón de salir.
     hecho = False
        
-
+    # Usado para gestionar cuán rápido se actualiza la pantalla.
     reloj = pygame.time.Clock() 
        
-    # -------- Bucle Principal del Programa  ----------- 
+    # -------- Bucle Principal del Programa ----------- 
     while not hecho: 
-        for evento in pygame.event.get(): 
-            if evento.type == pygame.QUIT: #Si el usuario hizo click en salir
+        for evento in pygame.event.get(): # El usuario realizó alguna acción 
+            if evento.type == pygame.QUIT:  # Si el usuario hizo click en salir
                 hecho = True # Marcamos como hecho y salimos de este bucle
      
             if evento.type == pygame.KEYDOWN:
@@ -262,11 +324,7 @@ def main():
                 if evento.key == pygame.K_RIGHT:
                     protagonista.ir_derecha()
                 if evento.key == pygame.K_UP:
-                    protagonista.saltar()
-		if evento.key == pygame.K_f:
-                    pygame.display.toggle_fullscreen()
-		if evento.key == pygame.K_s:
-                    hecho = True
+                    protagonista.jump()
                      
             if evento.type == pygame.KEYUP:
                 if evento.key == pygame.K_LEFT and protagonista.cambio_x < 0: 
@@ -277,7 +335,7 @@ def main():
         # Actualizamos al protagonista. 
         listade_sprites_activas.update()
          
-        # Actualizamos los objetos en el nivel
+        # Actualizamos los objetos en el Nivel
         nivel_actual.update()
          
         # Si el protagonista se aproxima al borde derecho, desplazamos el escenario a la izquierda(-x)
@@ -287,25 +345,23 @@ def main():
             nivel_actual.escenario_desplazar(-diff)
      
         # Si el protagonista se aproxima al borde izquierdo, desplazamos el escenario a la derecha(+x)
-        if protagonista.rect.x <= 120 and protagonista.rect.x>0:
+        if protagonista.rect.x <= 120:
             diff = 120 - protagonista.rect.x
             protagonista.rect.x = 120
             nivel_actual.escenario_desplazar(diff)
   
-       
-
+             
         nivel_actual.draw(pantalla)
         listade_sprites_activas.draw(pantalla)
-         
-
            
+
         # Limitamos a 60 fps
         reloj.tick(60) 
        
-        # Avanzamos y actualizamos la pantalla que ya hemos dibujado 
+        
         pygame.display.flip() 
            
-    
+
     pygame.quit()
  
 if __name__ == "__main__":
